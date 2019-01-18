9cb924bfe2a8b33f3e27fbaad7ff9e7fcf4fb8af
yhocn107 <yhocn107@163.com>
2019-01-18 16:13:03 +0800

    17.1
    17.1

--- miniprogram/pages/chanpin1/chanpin1.js
+++ miniprogram/pages/chanpin1/chanpin1.js
@@ -24,10 +24,10 @@ Page({
   },
   jiaru: function () {
     var that=this;
-    var open={}
+    
     var obj = wx.getStorageSync("openid")
-     open.id = that.data.id;
-    open.openid=obj.openid;
+    var id = that.data.id;
+    var openid=obj.openid;
     
     console.log(open)
     const db = wx.cloud.database();
@@ -37,25 +37,32 @@ Page({
         console.log(res.data)
         for (var i = 0; i < countResult;i++)
         {
-          var openidd = res.data[i]
+          var openidd = res.data[i]._openid
+          var idd = res.data[i].id
           console.log(openidd)
-          if(open==openidd){
+        //  if(id==){
+        //   db.collection('shoucang').add({
+        //         data: {
+        //           id: id
+        //              },
+        //         success: res => {
+        //           wx.showToast({
+        //     title: '收藏成功',
+        //     })
+        //    },
+        //   })
+        //   }
+          if ((id != idd) || (openid != openidd)) {
             wx.showToast({
               title: '您已收藏',
             })
           }
-          else if(open != openidd){
-          db.collection('shoucang').add({
-                data: {
-                  id: id
-                     },
-                success: res => {
-                  wx.showToast({
-            title: '收藏成功',
+          if ((id == idd)&&(openid==openidd)) {
+            wx.showToast({
+              title: '您已收藏',
             })
-           },
-          })
           }
+           
         } 
       }
      }
